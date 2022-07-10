5ad78daf73aef2ce799ab06cbde71a6d3759f4a8
CHUN-CHAO WANG <ccw@gm.ntpu.edu.tw>
2022-07-10 14:57:07 +0800

    to github

--- gar.py
+++ gar.py
@@ -1,15 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm, t
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.stats import norm, t
 
-fig, ax = plt.subplots()
-xlim = [-15, 15]
-mu = 0
-s = np.arange(1, 6)
-x = np.linspace(xlim[0], xlim[1], 1000)
-y = norm.pdf(x.reshape(-1, 1), mu, s)
-# print(y.shape)
-label = ["$\sigma$={}".format(i) for i in s]
-plt.plot(x, y, label=label)
-plt.legend()
-plt.show()
+# fig, ax = plt.subplots()
+# xlim = [-15, 15]
+# mu = 0
+# s = np.arange(1, 6)
+# x = np.linspace(xlim[0], xlim[1], 1000)
+# y = norm.pdf(x.reshape(-1, 1), mu, s)
+# # print(y.shape)
+# label = ["$\sigma$={}".format(i) for i in s]
+# plt.plot(x, y, label=label)
+# plt.legend()
+# plt.show()
+# import multiprocessing
+from playsound import playsound
+
+sound_file = "../multimedia_py39/taylor_music.wav"
+# p = multiprocessing.Process(target=playsound, args=(sound_file,))
+# p.start()
+playsound(sound_file)
+# input("press ENTER to stop playback")
+# p.terminate()
